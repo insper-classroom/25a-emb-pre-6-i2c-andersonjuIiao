789ce61c87e93c64e63d9e7f5dd89811ebd25a97
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-04-14 14:55:45 -0300

    ex1 - try 01

--- exe1/main.c
+++ exe1/main.c
@@ -15,21 +15,20 @@ const int I2C_SDA_GPIO = 20;
 const int I2C_SCL_GPIO = 21;
 
 void i2c_task(void *p) {
-    i2c_init(i2c_default, 400 * 1000);
+    i2c_init(i2c_default, 400000);
     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
     gpio_pull_up(I2C_SCL_GPIO);
 
-    // reset device to its default state
-    // colocando 1 no bit 7 do registrador 0x6B
     uint8_t buf_write[2];
-    buf_write[0] = MPUREG_PWR_MGMT_1; // registrador
-    buf_write[1] = 1 << 7;            // valor
+    buf_write[0] = MPUREG_PWR_MGMT_1;
+    buf_write[1] = 1 << 7;
     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
 
-    // TODO
-    // Configure o acc para operar em 4G
+    buf_write[0] = MPUREG_ACCEL_CONFIG;
+    buf_write[1] = 1 << 3;
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
@@ -43,6 +42,5 @@ int main() {
     xTaskCreate(i2c_task, "i2c task", 4095, NULL, 1, NULL);
     vTaskStartScheduler();
 
-    while (true) {
-    }
+    while (true) {}
 }
